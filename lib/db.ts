278b23ed956f50494e046dbcc0ab6a5cbf9f7c39
Nathali <nathalisocorro2708@gmail.com>
2026-01-16 23:28:30 -0500

    david's changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,18 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL
-})
+import { PrismaClient } from "@prisma/client"
+//import { PrismaClient } from "@/app/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { Pool } from "pg"
 
 const prismaClientSingleton = () => {
-    return new PrismaClient(
-        {
-            adapter
-        }
-    )
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+    const adapter = new PrismaPg(pool)
+    return new PrismaClient({ adapter })
 }
+
 declare const globalThis: {
     prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
+
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 export default prisma
